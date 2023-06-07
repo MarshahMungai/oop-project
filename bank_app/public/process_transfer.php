a35b066b45e9ca0ccad1242eef5754a27ab81707
Peter Macharia <mungaipeter640@gmail.com>
2023-06-07 09:14:45 +0300

    transaction added

--- bank_app/public/process_transfer.php
+++ bank_app/public/process_transfer.php
@@ -1,63 +1,59 @@
 <?php
-
-// Include necessary files and check if the user is logged in
-require_once __DIR__ . '/../config/db.php';
-require_once __DIR__ . '/../classes/User.php';
-require_once __DIR__ . '/../classes/UserManager.php';
+// Include UserManager class
+require_once '../classes/UserManager.php';
 
 session_start();
 
-// Check if the user is logged in
-if (!isset($_SESSION['user_id'])) {
-    // Redirect to the login page if the user is not logged in
-    header('Location: login.php');
+// Retrieve sender's user ID from the session
+$senderId = $_SESSION['user_id'];
+
+// Retrieve form inputs
+$recipientAccount = $_POST['recipient'];
+$transferAmount = $_POST['amount'];
+
+// Create an instance of UserManager
+$userManager = new UserManager();
+
+// Retrieve sender's account balance from the database
+$senderAccountNumber = $_SESSION['account_number']; // Assuming you have the sender's account number stored in the session
+$senderBalance = $userManager->getAccountBalance($senderAccountNumber);
+
+// Verify if sender has sufficient funds
+if ($senderBalance < $transferAmount) {
+    // Display error message to the user
+    echo "Insufficient funds. Transfer cannot be completed.";
     exit();
 }
 
-// Handle the transfer process
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['transfer'])) {
-        // Retrieve transfer details from the form
-        $recipient = $_POST['recipient'];
-        $amount = $_POST['amount'];
+// Get recipient's user ID by account number
+$recipientId = $userManager->getUserIdByAccountNumber($recipientAccount);
 
-        // Establish a database connection
-        $db = new DB();
-        $conn = $db->getConnection();
-
-        // Get the logged-in user's ID and validate it
-        $userID = $_SESSION['user_id'];
-
-        // Retrieve sender's account details from the database
-        $userManager = new UserManager($conn);
-        $sender = $userManager->getUserById($userID);
-
-        // Check if the sender has enough balance for the transfer
-        if ($amount > $sender->getBalance()) {
-            $_SESSION['transfer_error'] = "Insufficient balance.";
-            header('Location: dashboard.php');
-            exit();
-        }
-
-        // Retrieve recipient's account details from the database
-        $recipientUser = $userManager->getUserById($recipient);
-
-        if (!$recipientUser) {
-            $_SESSION['transfer_error'] = "Recipient not found.";
-            header('Location: dashboard.php');
-            exit();
-        }
-
-        // Perform the transfer
-        $userManager->transferFunds($sender, $recipientUser, $amount);
-
-        $_SESSION['transfer_success'] = true;
-        header('Location: dashboard.php');
-        exit();
-    }
+if (!$recipientId) {
+    // Display error message if recipient account does not exist
+    echo "Recipient account not found.";
+    exit();
 }
 
-// Redirect to the dashboard if the request method is not POST
-header('Location: dashboard.php');
+// Retrieve recipient's account balance from the database
+$recipientBalance = $userManager->getAccountBalance($recipientAccount);
+
+// Update sender's account balance
+$newSenderBalance = $senderBalance - $transferAmount;
+$userManager->updateAccountBalance($senderAccountNumber, $newSenderBalance);
+
+// Update recipient's account balance
+$newRecipientBalance = $recipientBalance + $transferAmount;
+$userManager->updateAccountBalance($recipientAccount, $newRecipientBalance);
+
+// Insert transaction details into the database
+$description = "Funds Transfer";
+$transactionDate = date("Y-m-d H:i:s");
+$userManager->insertTransaction($senderId, $recipientId, $transferAmount, $description, $transactionDate);
+
+// Display success message to the user
+echo "Transfer completed successfully.";
+
+// Redirect back to the transfer form or any other desired page
+header("Location: dashboard.php");
 exit();
 ?>
--- bank_app/views/dashboard.php
+++ bank_app/views/dashboard.php
@@ -3,9 +3,101 @@
 <head>
     <title>Banking Dashboard</title>
     <link rel="stylesheet" href="../public/assets/css/styles.css">
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f2f2f2;
+            padding: 20px;
+        }
+        
+        h1, h2 {
+            color: #333;
+        }
+        
+        h1 {
+            margin-top: 0;
+            margin-bottom: 20px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+        
+        h1 a {
+            background-color: white;
+            color: red;
+            padding: 4px 8px;
+            text-decoration: none;
+            border: ipx solid red;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        h1 a:hover {
+            background-color: red;
+            color: white;
+        }
+        
+        h2 {
+            margin-bottom: 10px;
+        }
+        
+        p {
+            margin-bottom: 5px;
+        }
+        
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-bottom: 20px;
+        }
+        
+        th, td {
+            padding: 10px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
+        
+        th {
+            background-color: #f2f2f2;
+        }
+        
+        form {
+            margin-bottom: 20px;
+            display: inline-block;
+        }
+        
+        label {
+            display: block;
+            margin-bottom: 5px;
+        }
+        
+        input[type="text"],
+        input[type="number"] {
+            width: 200px;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+        }
+        
+        button[type="submit"] {
+            padding: 10px 20px;
+            background-color: #4CAF50;
+            color: white;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+        
+        button[type="submit"]:hover {
+            background-color: #45a049;
+        }
+    </style>
 </head>
 <body>
-    <h1>Welcome to Your Banking Dashboard <span><a href="logout.php">Logout</a> <!-- Logout link or button --></span></h1>
+    <h1>
+        Welcome to Your Banking Dashboard
+        <a href="logout.php">Logout</a>
+    </h1>
 
     <h2>Account Summary</h2>
     <p>Account Number: <?= $accountNumber ?></p>
